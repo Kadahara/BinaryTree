7e531f109ffc3e6c0ae16f82cbcc19ad87c06ff9
User <User@WIN-E8F88FA2UKH>
2023-06-02 13:01:15 +0300

    Add project files.

---
+++ BinaryTree/Main.cpp
@@ -0,0 +1,135 @@
+#include<iostream>
+using namespace std;
+
+class Tree
+{
+protected:
+	class Element
+	{
+
+		int Data;
+		Element* pLeft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr)
+			:Data(Data), pLeft(pLeft), pRight(pRight)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor" << this << endl;
+		}
+		friend class Tree;
+		friend class UniqueTree;
+	}*Root;
+public:
+	Element* getRoot()
+	{
+		return Root;
+	}
+	Tree() :Root(nullptr)
+	{
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Tree()
+	{
+		cout << "TDestuctor:\t" << this << endl;
+	}
+
+	//					Adding elements:
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr) Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr) Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+	int minValue(Element* Root)const
+	{
+		if (Root == nullptr)return 0;
+		/*if (Root->pLeft == nullptr)return Root->Data;
+		else return minValue(Root->pLeft);*/
+		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+	}
+	//				Metods
+	int maxValue(Element* Root)const
+	{
+		if (Root == nullptr) return 0;
+		/*if (Root->pRight == nullptr)return Root->Data;
+		else return maxValue(Root->pRight);*/
+		return Root->pRight == nullptr ? Root->Data : maxValue(Root->pRight);
+	}
+	int count(Element* Root)const
+	{
+		if (Root == nullptr)return 0;
+		/*return count(Root->pLeft) + count(Root->pRight) + 1;*/
+		return !Root ? 0 : count(Root->pLeft) + count(Root->pRight) + 1;
+	}
+	void print(Element* Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << "\t";
+		print(Root->pRight);
+	}
+};
+
+class UniqueTree :public Tree
+{
+public:
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if (Data > Root->Data)
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int n;
+	cout << "Введите размер дерева: "; cin >> n;
+	Tree tree;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100, tree.getRoot());
+	}
+	tree.print(tree.getRoot());
+	cout << endl;
+	// minValue
+	cout << "Минимальное значение в дереве: " << tree.minValue(tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
+	cout << "Кол-во элементов в дереве: " << tree.count(tree.getRoot()) << endl;
+	cout << "------------------------------------------------" << endl;
+	UniqueTree u_tree;
+	for (int i = 0; i < n; i++)
+	{
+		u_tree.insert(rand() % 100, u_tree.getRoot());
+	}
+	u_tree.print(u_tree.getRoot());
+	cout << endl;
+	// minValue
+	cout << "Минимальное значение в дереве: " << u_tree.minValue(u_tree.getRoot()) << endl;
+	cout << "Максимальное значение в дереве: " << u_tree.maxValue(u_tree.getRoot()) << endl;
+	cout << "Кол-во элементов в дереве: " << u_tree.count(u_tree.getRoot()) << endl;
+}
